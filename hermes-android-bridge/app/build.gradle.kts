c10667a0c1b5622fe1359a6be96422c543868c98
JomWong <239300891@qq.com>
2026-08-19 20:52:34 +0800

    chore(bridge): bump to 0.5.4 (versionCode 8)

--- hermes-android-bridge/app/build.gradle.kts
+++ hermes-android-bridge/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.hermesandroid.bridge"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
-        versionName = "0.5.3"
+        versionCode = 8
+        versionName = "0.5.4"
     }
 
     buildFeatures {
